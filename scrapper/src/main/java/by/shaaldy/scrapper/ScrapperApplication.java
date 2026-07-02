9e6548d2487790f8dae0b24d5094b3aa3d87aec6
Shaaldy <138975611+Shaaldy@users.noreply.github.com>
2026-07-02 19:44:53 +0500

    spotless fix

--- scrapper/src/main/java/by/shaaldy/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/by/shaaldy/scrapper/ScrapperApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScrapperApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ScrapperApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(ScrapperApplication.class, args);
+  }
 }
