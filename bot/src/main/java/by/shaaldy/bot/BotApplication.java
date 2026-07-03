1b359b3112fd2fa3c08748431a7d47e31f9df0b6
Shaaldy <138975611+Shaaldy@users.noreply.github.com>
2026-07-03 14:22:25 +0500

    fix(bot): register AppProperties via @EnableConfigurationProperties

--- bot/src/main/java/by/shaaldy/bot/BotApplication.java
+++ bot/src/main/java/by/shaaldy/bot/BotApplication.java
@@ -2,8 +2,12 @@ package by.shaaldy.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import by.shaaldy.bot.config.AppProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class BotApplication {
 
   public static void main(String[] args) {
