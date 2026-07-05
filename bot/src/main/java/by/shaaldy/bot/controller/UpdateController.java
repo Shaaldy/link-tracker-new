cf6eac4a1653c56d0b62f6e86d2c32598c737f13
Shaaldy <138975611+Shaaldy@users.noreply.github.com>
2026-07-05 22:49:18 +0500

    fix(bot) endpoint /update

--- bot/src/main/java/by/shaaldy/bot/controller/UpdateController.java
+++ bot/src/main/java/by/shaaldy/bot/controller/UpdateController.java
@@ -1,26 +1,35 @@
 package by.shaaldy.bot.controller;
 
-import by.shaaldy.bot.dto.bot.LinkUpdate;
-import by.shaaldy.bot.telegram.MessageSender;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import by.shaaldy.bot.dto.bot.LinkUpdate;
+import by.shaaldy.bot.telegram.MessageSender;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class UpdateController {
 
-    private final MessageSender messageSender;
+  private final MessageSender messageSender;
 
-    @PostMapping("/updates")
-    public ResponseEntity<Void> receiveUpdate(@RequestBody LinkUpdate update) {
-        update.getTgChatIds().forEach(id ->
-                messageSender.send(id, "Обновление по ссылке " + update.getUrl() + "\n" + update.getDescription()));
-        log.info("Получено обновление ссылки {} для {} чатов", update.getUrl(), update.getTgChatIds().size());
-        return ResponseEntity.ok().build();
-    }
+  @PostMapping("/updates")
+  public ResponseEntity<Void> receiveUpdate(@RequestBody LinkUpdate update) {
+    update
+        .getTgChatIds()
+        .forEach(
+            id ->
+                messageSender.send(
+                    id,
+                    "Обновление по ссылке " + update.getUrl() + "\n" + update.getDescription()));
+    log.info(
+        "Получено обновление ссылки {} для {} чатов",
+        update.getUrl(),
+        update.getTgChatIds().size());
+    return ResponseEntity.ok().build();
+  }
 }
