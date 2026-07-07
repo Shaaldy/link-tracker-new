cc8b0e7baaecbdcb45c20d3ea8a9a452ea42d732
Shaaldy <138975611+Shaaldy@users.noreply.github.com>
2026-07-07 14:07:36 +0500

    feat(scrapper): add GitHub and StackOverflow update checkers

---
+++ scrapper/src/main/java/by/shaaldy/scrapper/client/github/GitHubClientConfig.java
@@ -0,0 +1,34 @@
+package by.shaaldy.scrapper.client.github;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import by.shaaldy.scrapper.config.AppProperties;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class GitHubClientConfig {
+
+  private final AppProperties properties;
+
+  @Bean
+  GitHubApi gitHubApi() {
+    AppProperties.GitHub github = properties.github();
+    RestClient.Builder builder =
+        RestClient.builder()
+            .baseUrl(github.baseUrl())
+            .defaultHeader("Accept", "application/vnd.github+json")
+            .defaultHeader("X-GitHub-Api-Version", "2022-11-28");
+    if (StringUtils.hasText(github.token())) {
+      builder.defaultHeader("Authorization", "Bearer " + github.token());
+    }
+    HttpServiceProxyFactory factory =
+        HttpServiceProxyFactory.builderFor(RestClientAdapter.create(builder.build())).build();
+    return factory.createClient(GitHubApi.class);
+  }
+}
---
+++ scrapper/src/main/java/by/shaaldy/scrapper/client/stackoverflow/StackOverflowClientConfig.java
@@ -0,0 +1,26 @@
+package by.shaaldy.scrapper.client.stackoverflow;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import by.shaaldy.scrapper.config.AppProperties;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class StackOverflowClientConfig {
+
+  private final AppProperties properties;
+
+  @Bean
+  StackOverflowApi stackOverflowApi() {
+    RestClient restClient =
+        RestClient.builder().baseUrl(properties.stackOverflow().baseUrl()).build();
+    HttpServiceProxyFactory factory =
+        HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+    return factory.createClient(StackOverflowApi.class);
+  }
+}
