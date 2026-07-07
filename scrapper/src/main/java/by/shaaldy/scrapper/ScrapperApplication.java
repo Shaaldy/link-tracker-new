c8cc2e39eddf1f6fef6854866e80b6c4672521ff
Shaaldy <138975611+Shaaldy@users.noreply.github.com>
2026-07-07 13:27:40 +0500

    fix(scrapper): register AppProperties bean and enable scheduling

--- scrapper/src/main/java/by/shaaldy/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/by/shaaldy/scrapper/ScrapperApplication.java
@@ -2,10 +2,14 @@ package by.shaaldy.scrapper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import by.shaaldy.scrapper.config.AppProperties;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class ScrapperApplication {
 
   public static void main(String[] args) {
