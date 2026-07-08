f40ace161d38c4ee14dd81042815e576d004a7ca
Shaaldy <138975611+Shaaldy@users.noreply.github.com>
2026-07-08 14:50:14 +0500

    feat(scrapper): configure datasource, disable liquibase autorun, add access-type    → application.properties, AppProperties

--- scrapper/src/main/java/by/shaaldy/scrapper/config/AppProperties.java
+++ scrapper/src/main/java/by/shaaldy/scrapper/config/AppProperties.java
@@ -1,21 +1,29 @@
 package by.shaaldy.scrapper.config;
 
-import java.time.Duration;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.validation.annotation.Validated;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import java.time.Duration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "app")
 public record AppProperties(
-    GitHub github,
-    StackOverflow stackOverflow,
-    @NotBlank String botBaseUrl,
-    @NotNull Duration schedulerInterval) {
+        GitHub github,
+        StackOverflow stackoverflow,
+        @NotBlank String botBaseUrl,
+        @NotNull AccessType accessType,
+        @NotNull Scheduler scheduler) {
+
   public record GitHub(@NotBlank String baseUrl, String token) {}
 
   public record StackOverflow(@NotBlank String baseUrl, String key) {}
+
+  public record Scheduler(@Positive int batchSize, @NotNull Duration interval) {}
+
+  public enum AccessType {
+    SQL,
+    ORM
+  }
 }
