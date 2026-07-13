c91317d895416f898adaa99856ca572f84891f7d
Shaaldy <138975611+Shaaldy@users.noreply.github.com>
2026-07-13 19:01:59 +0500

    feat(bot): /delete command

---
+++ bot/src/main/java/by/shaaldy/bot/command/DeleteCommand.java
@@ -0,0 +1,39 @@
+package by.shaaldy.bot.command;
+
+import by.shaaldy.bot.client.ScrapperApiException;
+import by.shaaldy.bot.client.ScrapperClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+
+@Component
+@RequiredArgsConstructor
+public class DeleteCommand implements Command{
+    private final ScrapperClient scrapperClient;
+
+    @Override
+    public String command() {
+        return "/delete";
+    }
+
+    @Override
+    public String description() {
+        return "Удалить чат и все подписки";
+    }
+
+    @Override
+    public String execute(long chatId, String text) {
+        try {
+            scrapperClient.deleteChat(chatId);
+            return "Чат и все подписки успешно удалены";
+        } catch (ScrapperApiException e) {
+            if (e.getStatus().value() == 404) {
+                return "Не удалось удалить: чат %d не зарегистрирован".formatted(chatId);
+            }
+            return "Не удалось удалить чат: %s".formatted(e.getMessage());
+        } catch (RestClientException e) {
+            return "Сервис временно недоступен, попробуйте позже.";
+        }
+    }
+}
