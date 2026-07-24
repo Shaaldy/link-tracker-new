33295a23846b470ee144881b766b54f42180ce26
Shaaldy <138975611+Shaaldy@users.noreply.github.com>
2026-07-24 13:03:28 +0500

    fix migration directory and paths

--- scrapper/src/main/java/by/shaaldy/scrapper/migration/LiquibaseMigrationRunner.java
+++ scrapper/src/main/java/by/shaaldy/scrapper/migration/LiquibaseMigrationRunner.java
@@ -16,6 +16,7 @@ import liquibase.command.core.helpers.DbUrlConnectionArgumentsCommandStep;
 import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.ClassLoaderResourceAccessor;
 import liquibase.resource.DirectoryResourceAccessor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -35,17 +36,13 @@ public class LiquibaseMigrationRunner {
 
   private final DataSource dataSource;
 
-  /** Каталог migrations/ относительно рабочей директории (корень проекта). */
-  @Value("${app.migrations.directory:migrations}")
-  private String migrationsDirectory;
-
-  /** Имя root changelog внутри каталога migrations/. */
-  @Value("${app.migrations.changelog:master.xml}")
+  /** Путь к root changelog В CLASSPATH (каталог migrations копируется в classpath через pom). */
+  @Value("${app.migrations.changelog:migrations/master.xml}")
   private String changelogFile;
 
   @EventListener(ApplicationReadyEvent.class)
   public void runMigrations() {
-    log.info("Running Liquibase migrations from {}/{}", migrationsDirectory, changelogFile);
+    log.info("Running Liquibase migrations from classpath: {}", changelogFile);
     try (Connection connection = dataSource.getConnection()) {
       Database database =
           DatabaseFactory.getInstance()
@@ -53,7 +50,7 @@ public class LiquibaseMigrationRunner {
 
       Scope.child(
           Scope.Attr.resourceAccessor,
-          new DirectoryResourceAccessor(new java.io.File(migrationsDirectory)),
+          new ClassLoaderResourceAccessor(getClass().getClassLoader()),
           () -> {
             new CommandScope("update")
                 .addArgumentValue(DbUrlConnectionArgumentsCommandStep.DATABASE_ARG, database)
