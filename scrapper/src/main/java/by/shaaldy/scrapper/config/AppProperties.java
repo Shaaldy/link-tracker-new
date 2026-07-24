89dfd7a59db94b75a06b6a16a9235f2390980433
Shaaldy <138975611+Shaaldy@users.noreply.github.com>
2026-07-24 18:47:46 +0500

    fix(scrapper): add @Valid annotation for correct configuration start

--- scrapper/src/main/java/by/shaaldy/scrapper/config/AppProperties.java
+++ scrapper/src/main/java/by/shaaldy/scrapper/config/AppProperties.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -12,17 +13,17 @@ import jakarta.validation.constraints.Positive;
 @Validated
 @ConfigurationProperties(prefix = "app")
 public record AppProperties(
-    GitHub github,
-    StackOverflow stackoverflow,
+    @Valid GitHub github,
+    @Valid StackOverflow stackoverflow,
     @NotBlank String botBaseUrl,
     @NotNull AccessType accessType,
-    @NotNull Scheduler scheduler) {
+    @Valid @NotNull Scheduler scheduler) {
 
   public record GitHub(@NotBlank String baseUrl, String token) {}
 
   public record StackOverflow(@NotBlank String baseUrl, String key) {}
 
-  public record Scheduler(@Positive int batchSize, @NotNull Duration interval, int parallelism) {}
+  public record Scheduler(@Positive int batchSize, @NotNull Duration interval, @Positive int parallelism) {}
 
   public enum AccessType {
     SQL,
