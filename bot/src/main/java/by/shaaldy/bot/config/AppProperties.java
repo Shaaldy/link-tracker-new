b3d7cf34360ce31f3f8d2a737675c7dc0924b5bf
Shaaldy <138975611+Shaaldy@users.noreply.github.com>
2026-07-27 13:16:29 +0500

    feat(bot): add Kafka producer dependencies and config

--- bot/src/main/java/by/shaaldy/bot/config/AppProperties.java
+++ bot/src/main/java/by/shaaldy/bot/config/AppProperties.java
@@ -1,10 +1,27 @@
 package by.shaaldy.bot.config;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 import jakarta.validation.constraints.NotBlank;
 
+import java.time.Duration;
+
 @Validated
 @ConfigurationProperties(prefix = "app")
-public record AppProperties(@NotBlank String telegramToken, @NotBlank String scrapperBaseUrl) {}
+public record AppProperties(@NotBlank String telegramToken, @NotBlank String scrapperBaseUrl, @NotNull MessageTransport messageTransport,
+                            @Valid @NotNull Kafka kafka,
+                            @Valid @NotNull Cache cache) {
+    public record Kafka(@Valid @NotNull Topics topics) {
+
+        public record Topics(@NotBlank String updates, @NotBlank String updatesDlq) {}
+    }
+    public record Cache(@NotNull Duration listTtl) {}
+
+    public enum MessageTransport {
+        KAFKA,
+        HTTP
+    }
+}
